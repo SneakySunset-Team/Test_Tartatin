5a41035ca05e51fac0a2305164dd11e3ef2a0a87
Sneakysunset <andrelauzier@gmail.com>
2025-11-10 13:47:32 +0100

    Upgrade system progress

---
+++ Assets/_Project/Scripts/Managers/TTUpgradeData.cs
@@ -0,0 +1,71 @@
+﻿using Sirenix.OdinInspector;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization.Tables;
+using UnityEngine.Serialization;
+
+public enum EUpgradeType {StartGold, TurretDamage, TurretFireRate, MineGoldPerTickAmount}
+
+[CreateAssetMenu(menuName = "UpgradeData")]
+public class TTUpgradeData : SerializedScriptableObject
+{
+    [field : SerializeField]
+    public Dictionary<EUpgradeType, SStatUpgradeData> upgradeData { get; private set; }
+
+    public void LevelUpStat(EUpgradeType upgradeType)
+    {
+        upgradeData[upgradeType].currentLevel++;
+    }
+
+    public int GetStatValue(EUpgradeType upgradeType)
+    {
+        var data = upgradeData[upgradeType];
+        return data.levelData[data.currentLevel].value;
+    }
+    
+    public bool IsUpgradeAvailable(EUpgradeType upgradeType)
+    {
+        var levelData= upgradeData[upgradeType].levelData;
+        int currentLevel = upgradeData[upgradeType].currentLevel;
+        return levelData != null && levelData.Length > currentLevel;
+    }
+
+    public bool CanUpgrade(EUpgradeType upgradeType)
+    {
+        var levelData= upgradeData[upgradeType].levelData;
+        int currentLevel = upgradeData[upgradeType].currentLevel;
+        return TTGameManager.Instance.currentDollarAmount >= levelData[currentLevel].levelUpCost;
+    }
+    
+    
+    #if UNITY_EDITOR
+    [OnInspectorInit]
+    private void Init()
+    {
+        upgradeData = new Dictionary<EUpgradeType, SStatUpgradeData>();
+        foreach (EUpgradeType upgradeType in Enum.GetValues(typeof(EUpgradeType)))
+        {
+            upgradeData.Add(upgradeType, new SStatUpgradeData());
+        }
+    }
+    #endif
+}
+
+[System.Serializable]
+public struct SStatLevelData
+{
+    [FoldoutGroup("Data")]
+    public int levelUpCost;
+    [FoldoutGroup("Data")]
+    public int value;
+}
+    
+[System.Serializable]
+public class SStatUpgradeData
+{
+    public SStatLevelData[] levelData;
+    public int currentLevel;
+    public Sprite icon;
+    public TableEntryReference _descriptionTableEntryReference;
+}
